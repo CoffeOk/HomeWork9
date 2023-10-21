d755e87f749fac134d77051f88bb0bd7b34aac18
WALL-E <you@example.com>
2023-10-21 21:50:10 +0300

    Решение задачи 66

---
+++ Task02(66)/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumNut(int x, int y)
+{
+    if (x == y) return x;
+    else return SumNut(x, y-1) + y;    
+}
+
+int n, m;
+Console.Write("Введите число: ");
+while (!int.TryParse(Console.ReadLine(), out m))
+    Console.WriteLine("Введите целое число");
+
+Console.Write("Введите число: ");
+while (!int.TryParse(Console.ReadLine(), out n))
+    Console.WriteLine("Введите целое число");
+
+int sum = SumNut (m, n);
+Console.WriteLine($"M = {m}; N = {n} -> {sum}");
